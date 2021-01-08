421d6e219f035a590cc3bfc12d4fc15e0428fccf
Allan Cheow <allan.cheow132@gmail.com>
2021-01-08 11:26:49 -0800

    Added filtering capabilities

--- app.js
+++ app.js
@@ -2,7 +2,39 @@
 
 const form = document.getElementById(`registrar`);
 const input = form.querySelector(`input`);
+
+const mainDiv = document.querySelector(`.main`);
 const ul = document.getElementById(`invitedList`);
+
+const div = document.createElement(`div`);
+const filterLabel = document.createElement(`label`);
+const filterCheckBox = document.createElement(`input`);
+
+filterLabel.textContent = `Hide those who haven't responded`;
+filterCheckBox.type = `checkbox`;
+div.appendChild(filterLabel);
+div.appendChild(filterCheckBox);
+mainDiv.insertBefore(div, ul);
+filterCheckBox.addEventListener(`change`, (e) => {
+    const isChecked = e.target.checked;
+    const lis = ul.children;
+    if (isChecked) {
+        for ( let i = 0; i < lis.length; i++ ) {
+            let li = lis[i];
+            if (li.className === `responded` ) {
+                li.style.display = ``;
+            } else {
+                li.style.display = `none`;
+            }
+        }
+    } else {
+        for ( let i = 0; i < lis.length; i++ ) {
+            let li = lis[i];
+            li.style.display = ``;
+        }
+    }
+});
+
 
 /**
  * Retrieves input from user to create 
@@ -15,7 +47,9 @@ const ul = document.getElementById(`invitedList`);
 
 function createLI(text) {
     const li = document.createElement(`li`);
-    li.textContent = text;
+    const span = document.createElement(`span`);
+    span.textContent = text;
+    li.appendChild(span);
     const label = document.createElement(`label`);
     label.textContent = `Confirmed`;
     // Adds checkbox
@@ -23,17 +57,21 @@ function createLI(text) {
     checkbox.type = `checkbox`;
     label.appendChild(checkbox);
     li.appendChild(label);
+    // Adds edit button
+    const editButton = document.createElement(`button`);
+    editButton.textContent = `edit`;
+    li.appendChild(editButton);
     // Adds remove button
-    const button = document.createElement(`button`);
-    button.textContent = `Remove`;
-    li.appendChild(button);
+    const removeButton = document.createElement(`button`);
+    removeButton.textContent = `remove`;
+    li.appendChild(removeButton);
     return li;
 };
 
 form.addEventListener(`submit`, (e) => {
     // Cancels browsers default submit behavior
     e.preventDefault();
-    //console.log(input.value);
+    // console.log(input.value);
     const text = input.value;
     input.value = ``;
     const li = createLI(text);
@@ -41,7 +79,7 @@ form.addEventListener(`submit`, (e) => {
 });
 
 ul.addEventListener(`change`, (e) => {
-    //console.log(e.target.checked);
+    // console.log(e.target.checked);
     const checkbox = e.target;
     const checked = checkbox.checked;
     const listItem = checkbox.parentNode.parentNode;
@@ -53,9 +91,29 @@ ul.addEventListener(`change`, (e) => {
 });
 
 ul.addEventListener(`click`, (e) => {
-    if (e.target.tagName === `BUTTON` ) {
-        const li = e.target.parentNode;
+    if ( e.target.tagName === `BUTTON` ) {
+        const button = e.target;
+        const li = button.parentNode;
         const ul = li.parentNode;
-        ul.removeChild(li);
+        if ( button.textContent === `remove` ) {
+            ul.removeChild(li);
+        } else if ( button.textContent === `edit` ) {
+            // console.log(`edit`);
+            const span = li.firstElementChild;
+            const input = document.createElement(`input`);
+            input.type = `text`;
+            input.value = span.textContent;
+            li.insertBefore(input, span);
+            li.removeChild(span);
+            button.textContent = `save`;
+        } else if ( button.textContent === `save` ) {
+            // console.log(`save`);
+            const input = li.firstElementChild;
+            const span = document.createElement(`span`);
+            span.textContent = input.value;            
+            li.insertBefore(span,input);
+            li.removeChild(input);
+            button.textContent = `edit`;
+        }
     }
 });
